241c7877dd8f03cd269a639547b693d3e9b5d029
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2024-01-12 11:50:38 +0800

    Getting started

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "very-dark-magenta": "hsl(300, 43%, 22%)",
+          "soft-pink": "hsl(333, 80%, 67%)",
+        },
+        neutral: {
+          "dark-grayish-magenta": "hsl(303, 10%, 53%)",
+          "light-grayish-magenta": "hsl(300, 24%, 96%)",
+          white: "hsl(0, 0%, 100%)",
+        },
+      },
+      fontFamily: {
+        "league-spartan": ["League Spartan", "sans-serif"],
+      },
+      fontSize: {
+        15: "15px",
+      },
+    },
   },
   plugins: [],
-}
+};
